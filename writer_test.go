dd3f4f94670552712bb815eb3bf1669747c9089b
Paul van Santen <pvansanten@transip.nl>
2019-11-26 10:25:19 +0100

    Add failing test for copying with a large buffer

--- writer_test.go
+++ writer_test.go
@@ -25,6 +25,7 @@ func TestWriterCopyFile(t *testing.T) {
 	}()
 
 	fileID, err := RandomFileID()
+	assert.NoError(t, err)
 	dst, err := ioutil.TempFile(os.TempDir(), "writer-copy-test-")
 	assert.NoError(t, err)
 	defer func() {
@@ -46,6 +47,61 @@ func TestWriterCopyFile(t *testing.T) {
 	}()
 
 	n, err := io.CopyBuffer(wrtr, src, make([]byte, 17))
+	assert.NoError(t, err)
+	assert.EqualValues(t, 117, n)
+
+	_, err = dst.Seek(0, io.SeekStart)
+	assert.NoError(t, err)
+	dstBuf, err := ioutil.ReadAll(dst)
+	assert.NoError(t, err)
+
+	_, err = src.Seek(0, io.SeekStart)
+	assert.NoError(t, err)
+	srcBuf, err := ioutil.ReadAll(src)
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, srcBuf, dstBuf)
+
+	assert.NoError(t, wrtr.Close())
+	assert.NoError(t, srv.Shutdown(context.Background()))
+}
+
+
+func TestWriterCopyFileLargeBuffer(t *testing.T) {
+	srv := NewFileServer(secret)
+	srv.SetLogger(&testLogger{t})
+
+	socket := newSocketPath()
+	sock, err := net.Listen("unix", socket)
+	assert.NoError(t, err)
+	go func() {
+		err = srv.Serve(sock)
+		assert.EqualValues(t, http.ErrServerClosed, err)
+	}()
+
+	fileID, err := RandomFileID()
+	assert.NoError(t, err)
+	dst, err := ioutil.TempFile(os.TempDir(), "writer-copy-test-")
+	assert.NoError(t, err)
+	defer func() {
+		_ = dst.Close()
+		_ = os.Remove(dst.Name())
+	}()
+
+	err = srv.ServeFileWriter(context.Background(), fileID, dst)
+	assert.NoError(t, err)
+
+	wrtr := NewCustomClientWriter(newHTTPUnixClient(socket), "http://server", secret, fileID)
+	wrtr.SetLogger(&testLogger{t})
+
+	src, err := randomFile(117_777)
+	assert.NoError(t, err)
+	defer func() {
+		_ = src.Close()
+		_ = os.Remove(src.Name())
+	}()
+
+	n, err := io.CopyBuffer(wrtr, src, make([]byte, 11_777))
 	assert.NoError(t, err)
 	assert.EqualValues(t, 117, n)
 
